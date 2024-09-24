e8e03cf78b8eef826c586ce150ac0ff2102f405c
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-09-24 11:38:40 +0300

    fix: modified the spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,10 @@
-import HashLoader from "react-spinners/HashLoader";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 export default function Loading() {
   return (
     <>
       <div className="flex items-center justify-center w-full h-full bg-green-400 text-neutral-200 absolute">
-        <HashLoader color="#ffffff" aria-label="Loading Spinner" data-testid="loader" />;
+        <ClimbingBoxLoader color="#ffffff" aria-label="Loading Spinner" data-testid="loader" />;
       </div>
     </>
   );
