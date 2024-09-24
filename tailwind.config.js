15abfcc615d9f498c7ce40c6c919f572f8c2aeb9
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-09-24 12:12:28 +0300

    feat: custom animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/*.jsx", "./src/components/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        move: {
+          from: {
+            opacity: 0.5,
+            transform: translateX("-100px"),
+          },
+          to: {
+            opacity: 1,
+            transform: translateX("0"),
+          },
+        },
+      },
+      animation: {
+        "animate-onscroll": {
+          animation: "move linear",
+          "animation-timeline": "view()",
+          "animation-range": "entry 0% cover 40%",
+        },
+      },
+    },
   },
   plugins: [],
 };
