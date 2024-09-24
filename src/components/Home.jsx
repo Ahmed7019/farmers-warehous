0e7762e06986310acadde81aec2f98daa0891d97
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-09-24 06:21:05 +0300

    feat: styling & images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,16 +8,19 @@ export default function Home() {
           <p className="font-bold text-4xl mb-8">Farmers Warehouse</p>
           <div className="w-32 h-3 rounded-sm bg-green-400 animate-pulse mb-4"></div>
           <div className=" my-2 flex justify-center">
-            <div className="flex flex-col gap-y-4 pr-4">
-              <h1>
+            <div className="flex flex-col gap-y-4 pr-4 bg-green-600 p-4 rounded-l-md">
+              <h1 className="text-3xl font-semibold text-white">
+                Protect Your Produce: Our Warehouse is Your Safe Haven
+              </h1>
+              <p className="font-thin text-neutral-200">
                 At Our Farmers Warehouse, we take pride in offering premium
                 storage solutions for your valuable crops. With a focus on
                 quality and excellence, we provide a secure and reliable
                 environment to store your produce, ensuring that it maintains
                 its freshness and value.
-              </h1>
-              <div className="flex flex-col gap-y-4 justify-start items-start">
-                <p>
+              </p>
+              <div className="flex flex-col gap-y-4 justify-start items-start ">
+                <p className="font-thin text-neutral-200">
                   We are a dedicated team committed to supporting farmers by
                   providing top-notch storage facilities for their crops. With
                   years of experience in the industry, we understand the
@@ -32,29 +35,34 @@ export default function Home() {
             <img
               src="https://plus.unsplash.com/premium_photo-1661849446191-8793e93a27c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODF8fGNyb3BzJTIwc3RvcmFnZXxlbnwwfHwwfHx8MA%3D%3D"
               alt="crops"
-              className="rounded-md"
+              className="rounded-r-md"
             />
           </div>
         </header>
 
         <main className="my-8">
           <section>
-            <div className="flex my-2 justify-center">
+            <div className="flex gap-x-8 my-2 justify-center bg-green-600 p-4 rounded-md">
               <img
                 src="https://plus.unsplash.com/premium_photo-1664297166040-4f5dd279f234?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGNyb3BzJTIwc3RvcmFnZXxlbnwwfHwwfHx8MA%3D%3D"
                 alt="storing crops"
                 className="rounded-md"
               />
-              <p className="pl-8">
-                Our primary goal is to offer farmers a trusted space to store
-                their crops with the highest standards of care. From
-                temperature-controlled environments to vigilant security
-                measures, we ensure that your produce remains in optimal
-                condition until it's ready for market. Experience peace of mind
-                knowing that your crops are in safe hands at Our Farmers
-                Warehouse. Let us help you safeguard your harvest and maintain
-                its quality for the market ahead.
-              </p>
+              <div>
+                <h2 className="text-3xl font-semibold text-white mb-4">
+                  Peace of Mind for Your Produce
+                </h2>
+                <p className="font-thin text-neutral-200">
+                  Our primary goal is to offer farmers a trusted space to store
+                  their crops with the highest standards of care. From
+                  temperature-controlled environments to vigilant security
+                  measures, we ensure that your produce remains in optimal
+                  condition until it's ready for market. Experience peace of
+                  mind knowing that your crops are in safe hands at Our Farmers
+                  Warehouse. Let us help you safeguard your harvest and maintain
+                  its quality for the market ahead.
+                </p>
+              </div>
             </div>
           </section>
 
@@ -73,7 +81,6 @@ export default function Home() {
               <p>&copy; 2024 Farmer's Warehouse. All Rights Reserved.</p>
             </div>
           </section>
-          
         </footer>
       </body>
     </>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,8 +5,12 @@ export default function Navigation() {
     <>
       <header className="mr-8">
         <nav className="flex flex-col gap-y-4 justify-center w-32 text-center bg-green-900 shadow-md backdrop-blur-lg text-neutral-100 fixed left-0 h-full my-8 font-bold rounded-lg overflow-hidden px-2">
-          <div>
-            <img src={Logo} alt="/" className="w-24 h-24 object-cover relative bottom-24 rounded-full" />
+          <div className="flex justify-center items-center">
+            <img
+              src={Logo}
+              alt="/"
+              className="w-24 h-24 object-cover relative bottom-24 rounded-full"
+            />
           </div>
           <Link
             to={"/"}
