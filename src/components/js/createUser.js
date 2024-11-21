089a596953c7a98ccd1a6f9a2760dc08d92fedba
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-11-21 11:06:49 +0200

    feat: createUser scripts

---
+++ src/components/js/createUser.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(firstName, lastName, password, email, gender, birthdate) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+    this.email = email;
+    this.gender = gender;
+    this.birthdate = birthdate;
+  }
+}
+
+export function createUser(fN, lN, pwd, email, gender, bd) {
+  const user = User(fN, lN, pwd, email, gender, bd);
+  console.log(user);
+  return user;
+}
