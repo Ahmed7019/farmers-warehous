25779acdfe65f1323633a051567976ffa2395ede
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2025-01-04 02:20:30 +0200

    feat:added unique ID to each user

--- src/components/js/Firebase/firestore.js
+++ src/components/js/Firebase/firestore.js
@@ -1,16 +1,19 @@
-import { collection, addDoc } from "firebase/firestore";
+import { collection, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
-
-export default async function addUsersToDatabase(user) {
+export default async function addUsersToDatabase(user, creds) {
   try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: user.firstName,
-      last: user.lastName,
-      email: user.email,
-      born: `${user.birthYear} ${user.birthMonth} ${user.birthday}`,
-      gender: user.gender,
-      id: Date.now(),
-    });
+    const docRef = await setDoc(
+      collection(db, "users", creds.uid),
+      {
+        first: user.firstName,
+        last: user.lastName,
+        email: user.email,
+        born: `${user.birthYear} ${user.birthMonth} ${user.birthday}`,
+        gender: user.gender,
+        id: Date.now(),
+      },
+      { merge: true }
+    );
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.log("Error", e);
