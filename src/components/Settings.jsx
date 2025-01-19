761abfe85b745b5a808aab49767d5c1bf8804439
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2025-01-20 00:23:41 +0200

    fix: fixed some code

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,43 +4,74 @@ import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { IoClose } from "react-icons/io5";
 import { FaSignOutAlt } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function Settings() {
-  const handleSignOut = () => {
-    doSignOut(auth);
+  const handleSignOut = async () => {
+    try {
+      await doSignOut(auth);
+      console.log("SignedOut Successfully");
+    } catch (error) {
+      console.error("Error Signing out", error.message);
+    }
   };
 
-  const [display, setDisplay] = useState("absolute");
+  const [isVisible, setisVisible] = useState(true);
+
+  // On pressing esc on keyboard hide the settings menu
+  const hideMenu = () => {
+    setisVisible(false);
+  };
+
+  const handleEsc = (event) => {
+    if (event.key === "Escape") hideMenu();
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleEsc);
+
+    return window.removeEventListener("keydown", handleEsc);
+    // clear event listener
+  }, []);
   return (
     <>
       <div
-        className={`${display} right-0 top-12 z-10 bg-neutral-200 p-3 rounded-lg transition-all`}
+        className={`${
+          isVisible ? "absolute" : "hidden"
+        } right-0 top-12 z-10 bg-neutral-200 p-3 rounded-lg transition-all`}
+        onBlur={() => hideMenu}
       >
         <ul className="flex flex-col gap-2 text-sm text-neutral-600">
           <div className="flex self-end">
             <button
               className="self-end text-xl"
-              onClick={() => setDisplay("hidden")}
+              onClick={hideMenu}
+              aria-label="Close settings menu"
             >
               <IoClose />
             </button>
           </div>
           <li>
-            <Link className="flex items-center gap-x-2 hover:text-neutral-900">
+            <Link
+              className="flex items-center gap-x-2 hover:text-neutral-900"
+              aria-label="Personal Information"
+            >
               Personal Information <CgProfile />
             </Link>
           </li>
           <li>
-            <Link className="flex items-center gap-x-2 hover:text-neutral-900">
+            <Link
+              className="flex items-center gap-x-2 hover:text-neutral-900"
+              aria-label="My Crops"
+            >
               My crops
             </Link>
-            <hr className="border-black my-2" />
           </li>
+          <hr className="border-black my-2" />
           <li className="text-center w-full">
             <Link
               onClick={handleSignOut}
               to={"../"}
               className="w-full p-2 rounded-md text-neutral-200 bg-red-600 flex items-center justify-center gap-x-2 hover:bg-red-500 hover:text-neutral-100"
+              aria-label="Logout"
             >
               <p>Logout</p> <FaSignOutAlt />
             </Link>
