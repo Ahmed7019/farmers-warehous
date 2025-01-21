ec696f82a792fd43867df1691203e2db3551ce7f
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2025-01-21 15:39:21 +0200

    fix: fixed deployments and react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,96 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
+import { lazy, Suspense } from "react";
+import Loading from "./components/Loading";
+import Signin from "./components/Signin";
+import { AuthProvider } from "./contexts/authContext";
 
-const router = createBrowserRouter([
+// Lazy load components
+const Home = lazy(() => import("./components/Home"));
+const Inventory = lazy(() => import("./components/Inventory"));
+const Navigation = lazy(() => import("./components/Navigation"));
+const AboutUs = lazy(() => import("./components/AboutUs"));
+const ContactUs = lazy(() => import("./components/ContactUs"));
+const Storages = lazy(() => import("./components/Storages"));
+const Signup = lazy(() => import("./components/Signup"));
+
+// Create the router
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <AuthProvider>
+          <Navigation />
+        </AuthProvider>
+      ),
+      children: [
+        {
+          index: true,
+          element: (
+            <Suspense fallback={<Loading />}>
+              <Home />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/Inventory",
+          element: (
+            <Suspense fallback={<Loading />}>
+              <Inventory />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/AboutUs",
+          element: (
+            <Suspense fallback={<Loading />}>
+              <AboutUs />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/ContactUs",
+          element: (
+            <Suspense fallback={<Loading />}>
+              <ContactUs />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/Storages",
+          element: (
+            <Suspense fallback={<Loading />}>
+              <Storages />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/Signup",
+          element: (
+            <Suspense fallback={<Loading />}>
+              <Signup />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/Signin",
+          element: (
+            <Suspense fallback={<Loading />}>
+              <Signin />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+  ],
   {
-    path: "*",
-    element: <App />,
-  },
-]).basename("/farmers-warehous");
+    basename: "/farmers-warehous", // Add basename for GitHub Pages
+  }
+);
 
+// Render the app
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
