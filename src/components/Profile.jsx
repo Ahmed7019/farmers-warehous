e51fb0f3a26c972f18b03c1991b358c52707c20f
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2025-01-23 14:04:49 +0200

    feat: LOCATION API - Added new LOCATION API to fetch and display user location data - Integrated location data with user profile component - Improved error handling for location services

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/authContext";
 import { getUser } from "./js/Firebase/firestore";
 import { Link } from "react-router-dom";
 export default function Profile() {
+  const [response, setResponse] = useState();
   const { currentUser } = useAuth();
+  const [isLoading, setisLoading] = useState(false);
+
+  const getLocation = async () => {
+    // Get user location using IP API
+    try {
+      await fetch("https://ipapi.co/json/").then((response) => {
+        response.json().then((jsonData) => {
+          console.log(jsonData);
+          setResponse(jsonData);
+        });
+      });
+    } catch {
+      (error) => {
+        console.log(error);
+      };
+    }
+  };
+
   useEffect(() => {
-    getUser(currentUser);
-  }, [currentUser]);
+    if (currentUser == null) {
+      setisLoading(true);
+    } else {
+      setisLoading(false);
+      getUser(currentUser);
+    }
+    getLocation();
+    console.log("Location", response);
+  }, [currentUser, response]);
   return (
     <>
       <div className="mt-24 mx-8">
         <p>Profile page</p>
-        <p className="capitalize">Name : {currentUser.displayName}</p>
-        <p>Location:</p>
+        <p className="capitalize">
+          Name : {currentUser == null ? "" : currentUser.displayName}
+        </p>
+        <p>Location: {response ? response.city : "N/A"}</p>
         <p></p>
         Farm Size: [Size in Acres/Hectares]
         <p>Storage Capacity: [Current Storage Details]</p>
         <p>Primary Crops: [List of Crops]</p>
         <div className="flex gap-2 items-center">
-          <p className="">Email : {currentUser.email}</p>
+          <p>Email : {isLoading ? "" : currentUser.email}</p>
           <p
             className={`${
               currentUser.emailVerified ? "text-green-400" : "text-red-600"
