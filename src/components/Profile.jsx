1848d4b296e3e9c2d7fc44a8a61ab599fa196896
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2025-01-23 20:58:07 +0200

    feat : PROFILE INFO
    - Added information to the user's profile
    - Handled errors may occur if the user info failed to load
    - Added some styling to some elements

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,10 +2,26 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/authContext";
 import { getUser } from "./js/Firebase/firestore";
 import { Link } from "react-router-dom";
+import Loading from "./Loading";
 export default function Profile() {
   const [response, setResponse] = useState();
   const { currentUser } = useAuth();
   const [isLoading, setisLoading] = useState(false);
+
+  const months = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JULY",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
 
   const getLocation = async () => {
     // Get user location using IP API
@@ -24,42 +40,52 @@ export default function Profile() {
   };
 
   useEffect(() => {
+    getLocation();
+  }, []);
+  useEffect(() => {
     if (currentUser == null) {
       setisLoading(true);
     } else {
       setisLoading(false);
       getUser(currentUser);
     }
-    getLocation();
-    console.log("Location", response);
-  }, [currentUser, response]);
+  }, [currentUser]);
   return (
     <>
+      {!currentUser && <Loading />}
       <div className="mt-24 mx-8">
         <p>Profile page</p>
         <p className="capitalize">
           Name : {currentUser == null ? "" : currentUser.displayName}
         </p>
-        <p>Location: {response ? response.city : "N/A"}</p>
-        <p></p>
+        <p className="font-bold">
+          Location: {response ? response.country_capital : "N/A"}
+        </p>
         Farm Size: [Size in Acres/Hectares]
         <p>Storage Capacity: [Current Storage Details]</p>
         <p>Primary Crops: [List of Crops]</p>
         <div className="flex gap-2 items-center">
-          <p>Email : {isLoading ? "" : currentUser.email}</p>
+          <p>Email : {!currentUser ? "..." : currentUser.email}</p>
+
           <p
             className={`${
-              currentUser.emailVerified ? "text-green-400" : "text-red-600"
+              currentUser && currentUser.emailVerified
+                ? "text-green-400"
+                : "text-red-600"
             } text-xs font-bold`}
           >
-            Email is {currentUser.emailVerified ? "" : "not"} verified !
+            {`Email is ${currentUser && currentUser.emailVerified ? "" : "not"}
+            verified !`}
           </p>
+
           <Link className="text-xs text-neutral-600 hover:underline underline-offset-2">
             verify now
           </Link>
         </div>
-        <p>Phone Number</p>
-        <p>Member since</p>
+        <p className="text-sm text-neutral-400">
+          Member since
+          {currentUser == null ? "" : Date(currentUser.validSince).slice(3, 15)}
+        </p>
         <h3>Why Your Profile Matters ?</h3>
         <p>
           Your profile is more than just a dashboard—it’s a reflection of your
