0fc88b92043c54f98f430e061ed08837538a697d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2025-02-02 15:00:35 +0200

    DEPLOYMENT

---
+++ assets/ContactUs-BhK3o-5H.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CIA97Huo.js";function e(){return t.jsx(t.Fragment,{children:t.jsx("div",{className:"relative grid mx-4 mt-20",children:t.jsx("div",{children:t.jsx("h1",{className:"font-bold",children:"Contact Us"})})})})}export{e as default};
